a24aca065ae54552449955f9bef131eba7c20496
Hennes <hennes.paul06@gmail.com>
2026-05-28 11:12:47 +0200

    Sollte funktionieren (Tests und Tokens)

--- src/main/java/highlighting/presets/MiniJavaTokens.java
+++ src/main/java/highlighting/presets/MiniJavaTokens.java
@@ -19,7 +19,13 @@ public final class MiniJavaTokens {
   public static List<Token> defaultTokens() {
     return List.of(
         // Example: string literals (students should define further tokens below)
-        Token.of(Pattern.compile("\"([^\"\\\\]|\\\\.)*\""), MiniJavaColours.STRING_LITERAL_COLOUR)
+        Token.of(Pattern.compile("\"([^\"\\\\]|\\\\.)*\""), MiniJavaColours.STRING_LITERAL_COLOUR),
+        Token.of(Pattern.compile("/\\*\\*[\\s\\S]*?\\*/"), MiniJavaColours.JAVADOC_COMMENT_COLOUR),
+        Token.of(Pattern.compile("'([^'\\\\]|\\\\.)'"), MiniJavaColours.CHAR_LITERAL_COLOUR),
+        Token.of(Pattern.compile(("\\b(package|import|class|public|private|final|return|null|new)\\b")), MiniJavaColours.KEYWORD_COLOUR),
+        Token.of(Pattern.compile("@Override"), MiniJavaColours.ANNOTATION_COLOUR),
+        Token.of(Pattern.compile("//[^\\n]*"), MiniJavaColours.LINE_COMMENT_COLOUR),
+        Token.of(Pattern.compile("/\\*[\\s\\S]*?\\*/"), MiniJavaColours.BLOCK_COMMENT_COLOUR)
 
         // TODO: Define additional tokens for MiniJava, e.g. character literals, keywords,
         // annotations, comments, identifiers, numbers, operators, etc.
