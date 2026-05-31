5b17a3c9d4b596e8c4fcc732daa3164563d774b0
Hennes <hennes.paul06@gmail.com>
2026-05-31 11:50:51 +0200

    Rename Regextest class to RegexTest

--- src/test/java/Regextest.java
+++ src/test/java/Regextest.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class Regextest {
+public class RegexTest {
 
     RegexHighlighter h = new RegexHighlighter();
 
