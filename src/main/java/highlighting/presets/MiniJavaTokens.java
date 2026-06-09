fa412ab54d9ec587cf5cba25765bab7cc7e907fd
Hennes <hennes.paul06@gmail.com>
2026-06-09 11:50:16 +0200

    Gemacht 1.1 ca 4h Todo 1.2 - 2.2

--- src/main/java/highlighting/presets/MiniJavaTokens.java
+++ src/main/java/highlighting/presets/MiniJavaTokens.java
@@ -6,17 +6,16 @@ import java.util.regex.Pattern;
 
 public final class MiniJavaTokens {
 
- 
   public static List<Token> defaultTokens() {
     return List.of(
         Token.of(Pattern.compile("\"([^\"\\\\]|\\\\.)*\""), MiniJavaColours.STRING_LITERAL_COLOUR),
         Token.of(Pattern.compile("/\\*\\*[\\s\\S]*?\\*/"), MiniJavaColours.JAVADOC_COMMENT_COLOUR),
         Token.of(Pattern.compile("'([^'\\\\]|\\\\.)'"), MiniJavaColours.CHAR_LITERAL_COLOUR),
-        Token.of(Pattern.compile(("\\b(package|import|class|public|private|final|return|null|new)\\b")), MiniJavaColours.KEYWORD_COLOUR),
-        Token.of(Pattern.compile("@Override\\b"), MiniJavaColours.ANNOTATION_COLOUR)
+        Token.of(
+            Pattern.compile("\\b(package|import|class|public|private|final|return|null|new)\\b"),
+            MiniJavaColours.KEYWORD_COLOUR),
+        Token.of(Pattern.compile("@Override\\b"), MiniJavaColours.ANNOTATION_COLOUR),
         Token.of(Pattern.compile("//[^\\n]*"), MiniJavaColours.LINE_COMMENT_COLOUR),
-        Token.of(Pattern.compile("/\\*[\\s\\S]*?\\*/"), MiniJavaColours.BLOCK_COMMENT_COLOUR)
-
-        );
+        Token.of(Pattern.compile("/\\*[\\s\\S]*?\\*/"), MiniJavaColours.BLOCK_COMMENT_COLOUR));
   }
 }
--- src/main/java/highlighting/regex/RegexHighlighter.java
+++ src/main/java/highlighting/regex/RegexHighlighter.java
@@ -3,23 +3,19 @@ package highlighting.regex;
 import highlighting.core.HighlightRegion;
 import highlighting.core.SyntaxHighlighter;
 import highlighting.presets.MiniJavaTokens;
-
 import java.util.ArrayList;
 import java.util.List;
 
+public class RegexHighlighter extends SyntaxHighlighter {
 
-public class RegexHighlighter extends SyntaxHighlighter  {
-
- 
   @Override
   public List<HighlightRegion> collectMatches(String text) {
-      var candidates = new ArrayList<HighlightRegion>();
-      for (var token : MiniJavaTokens.defaultTokens()) {
-          candidates.addAll(token.test(text));
-      }
-      return candidates;
+    var candidates = new ArrayList<HighlightRegion>();
+    for (var token : MiniJavaTokens.defaultTokens()) {
+      candidates.addAll(token.test(text));
+    }
+    return candidates;
   }
-
 
   @Override
   public List<HighlightRegion> resolveConflicts(List<HighlightRegion> normalized) {
